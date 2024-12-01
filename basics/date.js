8b45ada136e3102a61b59f261eb612934b2a491b
mani1117 <71132517+mani1117@users.noreply.github.com>
2024-12-01 12:46:49 +0000

    Date

---
+++ basics/date.js
@@ -0,0 +1,17 @@
+let mydate = new Date()
+//console.log(mydate.toString());  //Sun Dec 01 2024 12:31:29 GMT+0000 (Coordinated Universal Time)
+//console.log(mydate.toDateString());  //Sun Dec 01 2024
+//console.log(mydate.toLocaleString()); // 12/1/2024, 12:35:09 PM
+
+//console.log(typeof mydate);  // object
+
+//let myCreateDate = new Date(2023, 0 , 23)
+
+//console.log(myCreateDate.toDateString());
+
+//let myCreatedDate = new Date(2023, 0 , 23, 5, 3)
+//console.log(myCreatedDate.toLocaleString());
+
+let myCreatedDate = new Date("2023-01-10")
+console.log(myCreatedDate.toLocaleString());
+
